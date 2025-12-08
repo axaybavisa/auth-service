4ca714113cf2cf8212864bb8857ba194cb8a0883
AxayBavisa <akshaybavisha19@gmail.com>
2025-12-08 11:14:21 +0530

    Add file task

---
+++ auth_service/users/task.py
@@ -0,0 +1,39 @@
+import random
+from django.core.mail import send_mail
+from django.conf import settings
+from .models import PaswordResetToken
+
+from .models import EmailOTP
+
+# For Verify-OTP
+def generate_otp():
+    return f"{random.randint(100000, 999999)}"
+
+def send_otp_via_email(user):
+    EmailOTP.objects.filter(user=user, is_used=False).delete()  # Remove any existing OTPs for the user
+
+    code = generate_otp()
+    EmailOTP.objects.create(user=user, code=code)
+
+    subject = "Your Email Verification OTP"
+    message = f"Your OTP for email verification is: {code}\n\nit will expire in 10 minutes."
+    from_email = getattr(settings, 'DEFAULT_FROM_EMAIL', None)
+    recipient_list = [user.email]
+
+    send_mail(subject, message, from_email, recipient_list)
+
+
+# For Forgot Password Link
+def send_password_reset_email(user):
+    raw_token, token_obj = PaswordResetToken.generate(user) 
+
+    reset_url = f"{settings.FRONTED_URL}/reset-password/{raw_token}"
+
+    subject = "Reset Your Password"
+    message = f"Click The link below to rest password:\n\n{reset_url}"
+    from_email = getattr(settings, 'DEFAULT_FROM_EMAIL', None)
+    recipient_list = [user.email]
+
+    send_mail(subject, message, from_email, recipient_list)
+
+    return raw_token 
