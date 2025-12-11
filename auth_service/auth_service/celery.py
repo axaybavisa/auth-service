ddda493efd74a386802d86783afd77f51dc06817
AxayBavisa <akshaybavisha19@gmail.com>
2025-12-11 18:09:18 +0530

    celery update

--- auth_service/auth_service/celery.py
+++ auth_service/auth_service/celery.py
@@ -1,8 +1,18 @@
 import os
+
 from celery import Celery
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "auth_service.settings")
+# Set the default Django settings module for the 'celery' program.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'auth_service.settings')
 
-app = Celery("auth_service")
-app.config_from_object("django.conf:settings", namespace="CELERY")
+app = Celery('auth_service')
+
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# Load task modules from all registered Django apps.
 app.autodiscover_tasks()
+
+
+@app.task(bind=True, ignore_result=True)
+def debug_task(self):
+    print(f'Request: {self.request!r}')
